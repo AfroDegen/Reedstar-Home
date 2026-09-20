90d56b1905c7b9682b0e081021a824b2aad37ad7
Reedstar <afrodegen@outlook.com>
2026-09-20 21:20:56 +0100

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "reedstar",
+  "private": true,
+  "scripts": {
+    "dev": "vite",
+    "build": "vite build"
+  },
+  "dependencies": {
+    "vue": "^3.5.0"
+  },
+  "devDependencies": {
+    "vite": "^6.0.0"
+  }
+}
